ee9fdab8de2965b385b4c70f63e4a9557cf2f090
Rohit-kumar <60548779+rohit9123@users.noreply.github.com>
2025-04-02 15:18:01 +0530

    updated link for download

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -16,7 +16,7 @@ export default function Download() {
     setSuccess(false);
     
     try {
-      const response = await axios.get(`/api/download/${id}`, {
+      const response = await axios.get(`https://image-processing-420g.onrender.com/api/download/${id}`, {
         responseType: 'blob',
       });
 
